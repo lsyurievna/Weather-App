5d2a055f4241a0a7db1716d08636f3f544fc7ca1
lsyurievna <lsyurievna@mun.ca>
2024-11-30 00:23:17 +0300

    Buttons are now-semi functional

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 const questions = [
     {
         question: "When did Canada gain independence?",
-        answer: "1978"
+        answers: [
+            {text: "1978", correct: true},
+            {text: "1988", correct: false},
+            {text: "1991", correct: false},
+            {text: "1964", correct: false},
+    
+        ]
     }
 ]
 
@@ -16,10 +22,28 @@ function showQuestion(){
     questionElement.innerHTML = currentQuestion.question;
 
     //answer button and text display part
-    let button = document.createElement("button");
-    button.innerHTML = currentQuestion.answer;
-    answerButtons.appendChild(button);
+    for (let i = 0; i < currentQuestion.answers.length; i++){
+        let button = document.createElement("button");
+        button.innerHTML = currentQuestion.answers[i].text;
+        answerButtons.appendChild(button);
+        if (currentQuestion.answers[i].correct){
+            button.dataset.correct = currentQuestion.answers[i].correct;
+        }
 
+        button.addEventListener("click", selectAnswer);
+    }
+    
+}
+
+function selectAnswer(e){
+    let selectedButton = e.target;
+    let isCorrect = selectedButton.dataset.correct === "true";
+    if (isCorrect){
+        selectedButton.classList.add("correct");
+    }
+    else{
+        selectedButton.classList.add("incorrect");
+    }
 }
 
 showQuestion();
