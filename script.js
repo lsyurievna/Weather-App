6f9e548e77dcc13509fe72c9ab3bb39b53db0190
lsyurievna <lsyurievna@mun.ca>
2024-12-09 23:47:16 +0300

    Core functionality is ready

--- script.js
+++ script.js
@@ -1,97 +1,19 @@
+const checkBtn = document.querySelector(".search button");
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiKey = "f841142a9080b6b9d00b2cf70a7c19ba";
 
-const questions = [
-    {
-        question: "When did Canada gain independence?",
-        answers: [
-            {text: "1978", correct: true},
-            {text: "1988", correct: false},
-            {text: "1991", correct: false},
-            {text: "1964", correct: false},
-    
-        ]
-    },
-    {
-        question: "What is the biggest animal in the world?",
-        answers: [
-            {text: "African elephant", correct: false},
-            {text: "Giant giraffe", correct: false},
-            {text: "White whale", correct: true},
-            {text: "Mouse", correct: false},
-    
-        ]
-    }
-]
 
-let currentQuestionIndex = 0;
-let questionElement = document.getElementById("question");
-let answerButtons = document.getElementById("answer-buttons");
-let nextButton = document.getElementById("next-btn");
 
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-}
+async function checkWeather(){
+    let city = document.querySelector(".search input").value;
+    const response = await fetch(apiUrl+`&q=${city}`+`&apiKey=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
 
-function showQuestion(){
-    resetState();
-    //question display part
-    let currentQuestion = questions[currentQuestionIndex];
-    questionElement.innerHTML = currentQuestion.question;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+};
+ 
 
-    //answer button and text display part
-    for (let i = 0; i < currentQuestion.answers.length; i++){
-        let button = document.createElement("button");
-        button.innerHTML = currentQuestion.answers[i].text;
-        answerButtons.appendChild(button);
-        if (currentQuestion.answers[i].correct){
-            button.dataset.correct = currentQuestion.answers[i].correct;
-        }
-
-        button.addEventListener("click", selectAnswer);
-    }
-    
-}
-
-function selectAnswer(e){
-    let selectedButton = e.target;
-    let isCorrect = selectedButton.dataset.correct === "true";
-    if (isCorrect){
-        selectedButton.classList.add("correct");
-    }
-    else{
-        selectedButton.classList.add("incorrect");
-    }
-}
-
-function resetState(){
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
-}
-
-function handleNext(){
-    currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
-        showQuestion();
-    }
-    else{
-        resetState();
-        questionElement.innerHTML = "Congrats! Quiz is over!";
-        nextButton.innerHTML = "Play Again";
-    }
-}
-
-nextButton.addEventListener("click", ()=> {
-    if(currentQuestionIndex < questions.length){
-        handleNext();
-    }
-    else{
-        startQuiz();
-    }
-})
-
-startQuiz();
-
+checkBtn.addEventListener("click", ()=>{checkWeather()})
 
